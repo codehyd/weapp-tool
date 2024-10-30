18ac94dad5c5a6d6a5150c0ab5f17b4aa273887d
coderhyd <2303885822@qq.com>
2024-10-30 18:07:08 +0800

    refactor: Simplify popup component code

--- src/base-ui/popup/index.tsx
+++ src/base-ui/popup/index.tsx
@@ -6,7 +6,7 @@ import styles from "./index.module.less";
 interface IBaseuiPopupProps {
   show: boolean;
   setShow: () => void;
-  mode?: "top" | "bottom" | "left" | "right" | "center";
+  mode: "top" | "bottom" | "left" | "right" | "center";
   children?: React.ReactNode;
   radius?: number;
   titleChildren?: React.ReactNode;
@@ -15,29 +15,41 @@ interface IBaseuiPopupProps {
 const BasePopup = React.memo((props: IBaseuiPopupProps) => {
   // 弹出层样式
   const popupStyle = React.useMemo(() => {
-    let radius = props.radius || 14 + "rpx";
-    let style = {};
-    if (props.mode == "bottom") {
-      style["borderRadius"] = `${radius} ${radius} 0 0`;
-    } else if (props.mode == "top") {
-      style["borderRadius"] = `0 0 ${radius} ${radius}`;
-    } else if (props.mode == "left") {
-      style["borderRadius"] = `0 ${radius} ${radius} 0`;
-    } else if (props.mode == "right") {
-      style["borderRadius"] = `${radius} 0 0 ${radius}`;
-    } else if (props.mode == "center") {
-      style["borderRadius"] = `${radius}`;
+    const radius = props.radius || 14 + "rpx";
+    const style: React.CSSProperties = {};
+    switch (props.mode) {
+      case "bottom":
+        style.borderRadius = `${radius} ${radius} 0 0`;
+        break;
+      case "top":
+        style.borderRadius = `0 0 ${radius} ${radius}`;
+        break;
+      case "left":
+        style.borderRadius = `0 ${radius} ${radius} 0`;
+        break;
+      case "right":
+        style.borderRadius = `${radius} 0 0 ${radius}`;
+        break;
+      case "center":
+        style.borderRadius = radius;
+        break;
     }
     return style;
   }, [props.mode, props.radius]);
 
-  const titleChildrenEl = () => {
-    return (
-      <View className={`${styles.title_container} one-line`}>
-        {props.titleChildren}
-      </View>
-    );
-  };
+  const titleChildrenEl = () => (
+    <View className={`${styles.title_container} one-line`}>
+      {props.titleChildren}
+    </View>
+  );
+
+  const renderContent = () => (
+    <View className={styles[`${props.mode}_container`]}>
+      <ScrollView scroll-y style={{ height: "100%" }}>
+        {props.children}
+      </ScrollView>
+    </View>
+  );
 
   return (
     <Popup
@@ -52,50 +64,9 @@ const BasePopup = React.memo((props: IBaseuiPopupProps) => {
       {/* 关闭按钮 */}
       <Popup.Close placement="top-right" />
 
-      {/* left */}
-      {props.mode == "left" && (
-        <View className={styles.left_container}>
-          <ScrollView scroll-y style={{ height: "100%" }}>
-            {props.children}
-          </ScrollView>
-        </View>
-      )}
-
-      {/* center */}
-      {props.mode == "center" && (
-        <View className={styles.center_container}>
-          <ScrollView scroll-y style={{ height: "100%" }}>
-            {props.children}
-          </ScrollView>
-        </View>
-      )}
-
-      {/* right */}
-      {props.mode == "right" && (
-        <View className={styles.right_container}>
-          <ScrollView scroll-y style={{ height: "100%" }}>
-            {props.children}
-          </ScrollView>
-        </View>
-      )}
-
-      {/* top */}
-      {props.mode == "top" && (
-        <View className={styles.top_container}>
-          <ScrollView scroll-y style={{ height: "100%" }}>
-            {props.children}
-          </ScrollView>
-        </View>
-      )}
-
-      {/* bottom */}
-      {props.mode == "bottom" && (
-        <View className={styles.bottom_container}>
-          <ScrollView scroll-y style={{ height: "100%" }}>
-            {props.children}
-          </ScrollView>
-        </View>
-      )}
+      {/* 渲染内容 */}
+      {["left", "center", "right", "top", "bottom"].includes(props.mode) &&
+        renderContent()}
     </Popup>
   );
 });
